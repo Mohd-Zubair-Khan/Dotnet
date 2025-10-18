80ec259c0049dc32a511a4d8b169839a4d745c7d
Mohd Zubair Khan <mohd.zubairkhan190@gmail.com>
2025-10-19 01:48:06 +0530

    minor changes

--- MileStone1/IntroToC#/Datatypes/s1/obj/Debug/net8.0/AreaOfSquare.AssemblyInfo.cs
+++ MileStone1/IntroToC#/Datatypes/s1/obj/Debug/net8.0/AreaOfSquare.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AreaOfSquare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bb14bc710b7c9a78d8494fb84c2501814d7d8ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("AreaOfSquare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AreaOfSquare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
