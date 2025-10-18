0b5b86da4489b0fe67e6637b12558977a8a69bfb
Mohd Zubair Khan <mohd.zubairkhan190@gmail.com>
2025-10-19 02:19:02 +0530

    1

--- MileStone1/IntroToC#/Arrays/s8/obj/Debug/net8.0/s8.AssemblyInfo.cs
+++ MileStone1/IntroToC#/Arrays/s8/obj/Debug/net8.0/s8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("s8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfa54f27ada7f4e561a6454885c4a5996f90c8f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("s8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("s8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
